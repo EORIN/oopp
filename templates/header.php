ad420fad2c86c44faf27182ca6e313c75c4c6a37
EORIN <povorovegor@yandex.ru>
2023-12-18 15:57:54 +0300

    db

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой блог</title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+<table class="layout" >
+    <tr>
+        <td colspan="2" class="header">
+            Мой блог
+        </td>
+    </tr>
+    <tr>
+        <td>
